02537fd23e483a7a2df706e2a630f021bdd9d4a0
SubQk <108710578+SubQk@users.noreply.github.com>
2022-08-19 14:19:09 +1200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "AKIAQRTD7O7HK2FY2YH7"
-  secret_key = "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"
+  access_key = 
+  secret_key = 
 }
